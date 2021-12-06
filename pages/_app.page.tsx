9bb92be9536392dea16bc075d33d0bc398aee72b
wisotge <rnalstn0507@gmail.com>
2021-12-06 23:00:53 +0900

    add recoil

--- pages/_app.page.tsx
+++ pages/_app.page.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import '../styles/globals.scss';
 import type { AppProps } from 'next/app';
+import { RecoilRoot } from 'recoil';
 import { Hydrate, QueryClient, QueryClientProvider } from 'react-query';
 
 const WisotgeApp = function ({ Component, pageProps }: AppProps) {
   const [queryClient] = React.useState(() => new QueryClient());
 
   return (
-    <QueryClientProvider client={queryClient}>
-      <Hydrate state={pageProps.dehydratedState}>
-        <Component {...pageProps} />
-      </Hydrate>
-    </QueryClientProvider>
+    <RecoilRoot>
+      <QueryClientProvider client={queryClient}>
+        <Hydrate state={pageProps.dehydratedState}>
+          <Component {...pageProps} />
+        </Hydrate>
+      </QueryClientProvider>
+    </RecoilRoot>
   );
 };
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
       blue: colors.lightBlue,
       red: colors.rose,
       pink: colors.fuchsia,
-      white: colors.white
+      white: colors.white,
     },
     fontFamily: {
       sans: ['Graphik', 'sans-serif'],
